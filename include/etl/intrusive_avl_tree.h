2e82dae52b56fa253bb7f185674c2901c5b788d1
Sergei Shirokov <sergej.shirokov@gmail.com>
2026-02-06 18:08:11 +0200

    Added draft `etl::intrusive_avl_tree` class.

---
+++ include/etl/intrusive_avl_tree.h
@@ -0,0 +1,112 @@
+///\file
+
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+https://www.etlcpp.com
+
+Copyright(c) 2026 Sergei Shirokov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#ifndef ETL_INTRUSIVE_AVL_TREE_INCLUDED
+#define ETL_INTRUSIVE_AVL_TREE_INCLUDED
+
+#include "platform.h"
+#include "type_traits.h"
+#include "error_handler.h"
+#include "intrusive_links.h"
+
+#include <stddef.h>
+
+namespace etl
+{
+  //***************************************************************************
+  ///\ingroup intrusive_avl_tree
+  /// Base for intrusive AVL tree. Stores elements derived any ETL type that supports an 'etl_next' pointer member.
+  /// \tparam TLink  The link type that the value is derived from.
+  //***************************************************************************
+  template <typename TLink>
+  class intrusive_avl_tree_base
+  {
+  public:
+
+    // Node typedef.
+    typedef TLink link_type;
+
+  protected:
+
+    //*************************************************************************
+    /// Constructor
+    //*************************************************************************
+    intrusive_avl_tree_base()
+    {
+    }
+
+    //*************************************************************************
+    /// Destructor
+    //*************************************************************************
+    ~intrusive_avl_tree_base()
+    {
+    }
+  };
+
+  //***************************************************************************
+  ///\ingroup intrusive_avl_tree
+  /// An intrusive AVL tree. Stores elements derived from any type that supports an 'etl_next' pointer member.
+  /// \warning This tree cannot be used for concurrent access from multiple threads.
+  /// \tparam TValue The type of value that the tree holds.
+  /// \tparam TLink  The link type that the value is derived from.
+  //***************************************************************************
+  template <typename TValue, typename TLink>
+  class intrusive_avl_tree : public etl::intrusive_avl_tree_base<TLink>
+  {
+  public:
+
+    // Node typedef.
+    typedef typename etl::intrusive_avl_tree_base<TLink> link_type;
+
+    // STL style typedefs.
+    typedef TValue            value_type;
+    typedef value_type*       pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type&       reference;
+    typedef const value_type& const_reference;
+    typedef size_t            size_type;
+
+    //*************************************************************************
+    /// Constructor
+    //*************************************************************************
+    intrusive_avl_tree()
+      : intrusive_avl_tree_base<TLink>()
+    {
+    }
+
+  private:
+
+    // Disable copy construction and assignment.
+    intrusive_avl_tree(const intrusive_avl_tree&);
+    intrusive_avl_tree& operator = (const intrusive_avl_tree& rhs);
+  };
+}
+
+#endif
